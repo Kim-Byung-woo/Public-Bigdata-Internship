a4ea155fb9cd713bc7e8228b17cfdc4fd02e802e
Kim-Byung-Woo <kbw1994@naver.com>
2020-10-05 17:14:11 +0900

    index 수정

--- src/debug_test.py
+++ src/debug_test.py
@@ -1,3 +1,3 @@
-for idx in range(2, 10):
-    for jdx in range(1, 10):
-        print(f'{idx} x {jdx} = {idx * jdx}')
+for i in range(2, 10):
+    for j in range(1, 10):
+        print(f'{i} x {j} = {i * j}')
